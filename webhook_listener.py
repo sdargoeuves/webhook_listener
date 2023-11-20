6bf3d6d50cdc8a39bf934969c5e20537942abbd7
Seb d'Argoeuves <sebastien.dargoeuves@ipfabric.io>
2023-11-20 12:41:11 +0000

    ngrok with settings

--- webhook_listener.py
+++ webhook_listener.py
@@ -2,7 +2,7 @@ import asyncio
 import json
 import sys
 from datetime import datetime, timezone
-from pydantic import BaseSettings
+from pydantic_settings import BaseSettings
 from typing import List
 
 import uvicorn
